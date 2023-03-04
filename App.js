67db18d0689d4360e77d37182ea3fdc2eebbc7ab
Anirudhwa Ankon <ankon495@gmail.com>
2023-03-05 04:18:39 +0600

    done

--- App.js
+++ App.js
@@ -36,17 +36,17 @@ export default function App() {
       });
   };
 
-  image1 = "https://img.freepik.com/free-photo/weather-effects-composition_23-2149853295.jpg?w=1380&t=st=1677953183~exp=1677953783~hmac=363522694a133c9e162598953798f5950cbffcc6dd27f62d416a28ab8f62ffc5";
+  const image1 = "https://img.freepik.com/free-photo/weather-effects-composition_23-2149853295.jpg?w=1380&t=st=1677953183~exp=1677953783~hmac=363522694a133c9e162598953798f5950cbffcc6dd27f62d416a28ab8f62ffc5";
 
-  image2 = `https://images.unsplash.com/photo-1532178910-7815d6919875?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80`;
+  const image2 = `https://images.unsplash.com/photo-1532178910-7815d6919875?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80`;
 
-  image3 = `https://images.unsplash.com/photo-1518467946652-b194dd6dd321?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80`;
+  const image3 = `https://images.unsplash.com/photo-1518467946652-b194dd6dd321?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80`;
 
-  image4 = `https://wallpapercave.com/wp/wp8973745.jpg`;
+  const image4 = `https://wallpapercave.com/wp/wp8973745.jpg`;
 
-  image5 = `https://images.unsplash.com/photo-1551234250-d88208c2ce14?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTB8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60`;
+  const image5 = `https://images.unsplash.com/photo-1551234250-d88208c2ce14?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTB8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60`;
 
-  image6 = `https://cdn.pixabay.com/photo/2019/08/19/07/45/corgi-4415649_960_720.jpg`;
+  const image6 = `https://cdn.pixabay.com/photo/2019/08/19/07/45/corgi-4415649_960_720.jpg`;
 
   let imageUrl = `https://images.pexels.com/photos/159020/sunset-sky-afterglow-evening-sky-159020.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`;
   if (conditionCode > 499 && conditionCode < 532) {
